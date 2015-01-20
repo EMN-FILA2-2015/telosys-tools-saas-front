e31b17e6f483349b07d69d31bfaff7bc91b6e89e
Ludovic Chaboud <l.chaboud@gmail.com>
2015-01-20 14:13:36 +0100

    restangular

--- client/app/app.js
+++ client/app/app.js
@@ -5,11 +5,14 @@ angular.module('telosysToolsSaasFrontApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'restangular'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, RestangularProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    RestangularProvider.setBaseUrl('http://localhost:8080');
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,9 +1,9 @@
   'use strict';
 
 angular.module('telosysToolsSaasFrontApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope, $http, Restangular) {
 
+    /*
     var req = {
       method: 'GET',
       url: 'http://localhost:8080/hello',
@@ -17,4 +17,24 @@ angular.module('telosysToolsSaasFrontApp')
     $http(req).success(function(msg) {
       $scope.msg = msg;
     });
+    */
+/*
+    // First way of creating a Restangular object. Just saying the base URL
+    Restangular
+      .oneUrl('server', 'http://localhost:8080/hello')
+      .get()
+      .then(function(hello) {
+        $scope.msg = hello;
+      }).catch(function (e) {
+        console.log('error : ' + e);
+      });
+*/
+    Restangular
+      .one('hello', 'John')
+      .get()
+      .then(function(hello) {
+        $scope.msg = hello;
+      }).catch(function (e) {
+        console.log('error : ' + e);
+      });
   });

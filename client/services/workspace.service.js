ef04b4a0ed99ea9dd5421832074a2de63a30bae3
Adrian Fraisse <adrian.fraisse@etudiant.mines-nantes.fr>
2015-05-10 12:50:05 +0200

    Services pour la gestion du workspace

---
+++ client/services/workspace.service.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Adrian on 10/05/2015
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('telosysToolsSaasFrontApp')
+        .factory('WorkspaceService', WorkspaceService);
+
+    WorkspaceService.$inject = ['Restangular', 'Logger'];
+    // look for parameter passing the id ?
+    /* @ngInject */
+    function WorkspaceService(Restangular, Logger) {
+        var service = Restangular.service('projects');
+        var logger = Logger.getInstance("WorkspaceService");
+
+        return {
+          get: get,
+          createFile: createFile,
+          getFile: getFileContent
+        };
+
+        ////////////////
+
+      function get(id) {
+        logger.debug('call the get /projects/id/workspace service');
+        return service.one(id).one('workspace').get()
+          .then(function(data) {
+            return data;
+          })
+          .catch(function(error) {
+            logger.error('get', 'Error during the get /projects/id/workspace call', error);
+          });
+      }
+
+      function createFile(id, path, content) {
+        logger.debug('call post /projects/id/workspace/files service');
+        return service.one(id).one('files').post({"path":path, "content": content})
+          .then(function(data) {
+            return data;
+          })
+          .catch(function(error) {
+            logger.error('createFile', 'Error during post /projects/id/workspace/files service', error);
+          });
+      }
+
+      function getFileContent(path) {
+        logger.debug('call get /projects/id/workspace/files service');
+        return service.one('files').doGET({"path": path})
+          .then(function(data) {
+            return data;
+          })
+          .catch(function(error) {
+            logger.error('getFileContent', 'Error during get /projects/id/workspace/files service', error);
+          });
+      }
+
+    }
+})();

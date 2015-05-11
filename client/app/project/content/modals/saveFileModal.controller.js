fc239d543101e6aac9ceab64a7850cf7e8a41197
Marion Bechennec <marion@dictanova.com>
2015-05-11 13:17:04 +0200

    Refactoring - changing controller and html file names from addFile*** to addFileModal***

--- client/app/project/content/modals/saveFileModal.controller.js
+++ client/app/project/content/modals/saveFileModal.controller.js
@@ -3,22 +3,22 @@
 
     angular
         .module('telosysToolsSaasFrontApp')
-        .controller('SaveFileController', SaveFileController);
+        .controller('SaveFileModalController', SaveFileModalController);
 
-    SaveFileController.$inject = ['$modalInstance', 'file'];
+    SaveFileModalController.$inject = ['$modalInstance', 'file'];
 
     /* @ngInject */
-    function SaveFileController($modalInstance, file) {
+    function SaveFileModalController($modalInstance, file) {
         /* jshint validthis: true */
         var vm = this;
         vm.filename = file;
 
         vm.ok = function() {
           $modalInstance.close();
-        };
+        }
         vm.cancel = function() {
           $modalInstance.dismiss('cancel');
-        };
+        }
     }
 
 })();
